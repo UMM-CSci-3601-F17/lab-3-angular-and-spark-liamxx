435258d06e52adf0af999c99a949a7c0b625f417
plumstyle <anxxx154@morris.umn.edu>
2017-12-12 21:28:16 -0600

    Finish all filters for todos and front-end access

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -10,12 +10,15 @@ import {Todo} from "./todo";
 
 export class TodoListComponent implements OnInit {
     //These are public so that tests can reference them (.spec.ts)
-    complete : boolean = false;
-    incomplete : boolean = false;
+    todoOwner : string;
+    todoCategory : string;
+    todoStatus : boolean;
+    todoBodyText : string;
+    todoLimit : number;
     public todos: Todo[];
     public filteredTodos: Todo[];
 
-    //Inject the UserListService into this component.
+    //Inject the TodoListService into this component.
     //That's what happens in the following constructor.
     //
     //We can call upon the service for interacting
@@ -23,32 +26,50 @@ export class TodoListComponent implements OnInit {
     constructor(private todoListService: TodoListService) {
 
     }
-/** filters samples
-    public filterTodos(searchName: string, searchAge: number): Todo[] {
+
+    public filterTodos(searchOwner: string,
+                       searchCategory: string,
+                       searchBody: string,
+                       searchLimit: number,
+                       searchStatus: boolean): Todo[] {
 
         this.filteredTodos = this.todos;
 
-        //Filter by name            change
-        if (searchName != null) {
-            searchName = searchName.toLocaleLowerCase();
+        // Filter by owner
+        if (searchOwner != null) {
+            searchOwner = searchOwner.toLocaleLowerCase();
 
             this.filteredTodos = this.filteredTodos.filter(todo => {
-                return !searchName || todo.name.toLowerCase().indexOf(searchName) !== -1;
+                return !searchOwner || todo.owner.toLowerCase().indexOf(searchOwner) !== -1;
             });
         }
-*
-*
-*
-        //Filter by age         change
-        if (searchAge != null) {
+
+        // Filter by category
+        if (searchCategory != null) {
             this.filteredTodos = this.filteredTodos.filter(todo => {
-                return !searchAge || todo.age == searchAge;
+                return !searchCategory || todo.category.toLowerCase().indexOf(searchCategory) !== -1;
             });
+        }
+
+        // Filter by body
+        if (searchBody != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchBody || todo.body.toLowerCase().indexOf(searchBody) !== -1;
+            });
+        }
+        // Filter with limit numbers
+        if (searchLimit != null) {
+            this.filteredTodos = this.filteredTodos.slice(0,searchLimit);
+        }
+        // Filter by status
+        if (searchStatus != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return todo.status == searchStatus;
+            })
         }
 
         return this.filteredTodos;
     }
- */
 
     ngOnInit(): void {
         //Get Users returns an Observable, basically a "promise" that
